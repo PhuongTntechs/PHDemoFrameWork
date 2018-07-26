d8966d73137bc3adc5163bcd743b18b18f6eb0b1
PhuongTntechs <kid.tn01@gmail.com>
2018-07-26 14:59:14 +0700

    Finished PHDemoFrameWork

---
+++ PHDemoFrameWork/PHDemoFrameWork/PHDemoFrameWork.h
@@ -0,0 +1,19 @@
+//
+//  PHDemoFrameWork.h
+//  PHDemoFrameWork
+//
+//  Created by phuongpro Imac on 7/26/18.
+//  Copyright © 2018 phuongpro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PHDemoFrameWork.
+FOUNDATION_EXPORT double PHDemoFrameWorkVersionNumber;
+
+//! Project version string for PHDemoFrameWork.
+FOUNDATION_EXPORT const unsigned char PHDemoFrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PHDemoFrameWork/PublicHeader.h>
+
+
---
+++ PHDemoFrameWork/PHDemoFrameWork/Service.swift
@@ -0,0 +1,18 @@
+//
+//  Service.swift
+//  PHDemoFrameWork
+//
+//  Created by phuongpro Imac on 7/26/18.
+//  Copyright © 2018 phuongpro. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    
+    private init() {}
+    
+    public static func doSomething() -> String {
+        return "Do say say something"
+    }
+}
